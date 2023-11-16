fbf0fb0bbe2c342277861c5a425dc69f409c1e2d
Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
2023-11-17 00:32:08 +0300

    feat: add golangcilint
    Add golangci lint to check on linting issues.
    Also add copyright and licenses for every file

--- tui/tui.go
+++ tui/tui.go
@@ -1,13 +1,28 @@
+// Copyright (c) 2023 0x6flab
+//
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
 package tui
 
 import (
 	"context"
 
+	"github.com/0x6flab/dtop/tui/views"
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/docker/docker/client"
-	"github.com/rodneyosodo/dtop/tui/views"
 )
 
 type state uint8
@@ -27,31 +42,31 @@ type Model struct {
 	state  state
 }
 
-func NewModel(ctx context.Context) (Model, error) {
+func NewModel(ctx context.Context) (*Model, error) {
 	client, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		return Model{}, err
+		return &Model{}, err
 	}
 
 	containerTable, err := views.ListContainers(ctx, client)
 	if err != nil {
-		return Model{}, err
+		return &Model{}, err
 	}
 
-	return Model{
+	return &Model{
 		client: client,
 		table:  containerTable,
 		state:  listContainers,
 	}, nil
 }
 
-func (m Model) Init() tea.Cmd { return nil }
+func (m *Model) Init() tea.Cmd { return nil }
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
+
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		switch keyMsg.String() {
 		case "esc":
 			if m.table.Focused() {
 				m.table.Blur()
@@ -66,10 +81,12 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			)
 		}
 	}
+
 	m.table, cmd = m.table.Update(msg)
+
 	return m, cmd
 }
 
-func (m Model) View() string {
+func (m *Model) View() string {
 	return baseStyle.Render(m.table.View()) + "\n"
 }
